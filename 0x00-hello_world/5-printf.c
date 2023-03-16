976de0a238353a2f6dc53d55334f14942457ff09
Esechie <emmanuelesechie@gmail.com>
2023-03-16 12:48:50 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - Program that prints with the function printf()
  * Return: 0 (success)
- */
+ */i
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

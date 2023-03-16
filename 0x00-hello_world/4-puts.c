f4768f41e6b463f83b5e80ca3e440dc6a3187b3c
Esechie <emmanuelesechie@gmail.com>
2023-03-16 12:23:22 -0700

    PUT

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/* 
+/**
  * A program that prints a line with pput function
- *
+ *Return: 0 (success)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }

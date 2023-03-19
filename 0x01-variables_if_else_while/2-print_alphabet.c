c034a057cca7efdc4984fb7dd5147ed611c3148e
Esechie <emmanuelesechie@gmail.com>
2023-03-18 18:01:11 -0700

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Void i.e function is null
+ * Return (0)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar ('\n');
+	return (0);
+}

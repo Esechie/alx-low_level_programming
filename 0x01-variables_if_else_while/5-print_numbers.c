7ba519d9d6360d9a60b9b4cf34265538fc7b04a5
Esechie <emmanuelesechie@gmail.com>
2023-03-19 08:19:49 -0700

    Base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Digit of base ten
+ * Return: success (0)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+	putchar('\n');
+}

5bf3fb08426e6c91265fcdd809ef3dddaba719da
Esechie <emmanuelesechie@gmail.com>
2023-03-19 08:09:07 -0700

    Exclusion

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Lowercase and omitting g
+ * Return: Success (0)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	return (0);
+}

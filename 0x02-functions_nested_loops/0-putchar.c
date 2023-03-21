a69f81a2cfe1d6829bf54c0a27baf486ca92989e
Esechie <emmanuelesechie@gmail.com>
2023-03-21 16:42:17 -0700

    _{UTCHAR

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - prints -putchar to the screen
+ * main -putchar to the screen
+ * _putchar: prints the alphabet putchar
  * Return: success (0)
  */
 int main(void)
 {
-	printf("_putchar");
+	_putchar("_");
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar("\n");
 	return (0);
 }

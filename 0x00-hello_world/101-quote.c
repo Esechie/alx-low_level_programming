a477ce01d99e75c6e863ef9c14c171e91cc7ee54
Esechie <emmanuelesechie@gmail.com>
2023-03-20 19:46:05 -0700

    Print line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints text along side with date.
- * Return 1 (success)
+ * main - A program that prints text along side with date
+ * Return: Successful (1)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }

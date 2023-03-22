9ab5dfee1c611e299b5515a251888cfa4b78d62c
Esechie <emmanuelesechie@gmail.com>
2023-03-22 07:01:12 -0700

    Lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,13 @@
 #include "main.h"
 /**
- * _islower - displays lowest alphabet on the screent
- * @C - is the char to be checked 
- * Return: success (1)
+ *_islower - displays lowest alphabet on the screent
+ *@c: is the char to be checked
+ *Return: success (1)
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	{
 		return (1);
-	} 
 	else
-	{
 		return (0);
-	}
 }

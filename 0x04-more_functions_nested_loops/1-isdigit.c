2893714a6c13126b15e3e3f626c3f27b7719298b
Esechie <emmanuelesechie@gmail.com>
2023-03-23 09:45:01 -0700

    check for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - check for digits
+ * Return: 1 if true, else return 0
+ */
+int _isdigit(int x)
+{
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}

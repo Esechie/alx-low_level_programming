8c6f3ccb4ec1ec6b9201a1dfc003546de538a904
Esechie <emmanuelesechie@gmail.com>
2023-03-23 10:09:56 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - display numbers fro zero to nine
+ * return: 0
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	if (i <= 48 && i <= 57)
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

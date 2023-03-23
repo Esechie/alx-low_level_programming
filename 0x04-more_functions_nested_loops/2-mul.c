8d1c085f7245f97fcbce9a229fee43dfff95bd87
Esechie <emmanuelesechie@gmail.com>
2023-03-23 09:57:48 -0700

    Compute both numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - compute
+ *@a: first valuse
+ *@b: value to be multiplied with a
+ *Return: x
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+	return (x);
+}

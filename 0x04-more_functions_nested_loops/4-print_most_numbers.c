def00d67ce002b780036a42b9a3132660002e240
Esechie <emmanuelesechie@gmail.com>
2023-03-23 10:39:30 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - print the numbers zero to nine
+ * Description: prints the numbers excluding 2 and 4
+ * Return: The numbers since zero up to nine
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+}
+

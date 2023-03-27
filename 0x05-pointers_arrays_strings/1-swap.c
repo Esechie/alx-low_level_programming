e74d98409a26ad66d4bf3cd80992d223dfbc5aac
Esechie <emmanuelesechie@gmail.com>
2023-03-27 07:53:57 -0700

    Swapped Numbers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- * void swap_int - Function to hold changed docket
+ * swap_int - Function to hold changed docket
  * @a: first focus
  * @b: second focus
  * return: success
  */
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int s;
+
+	s = *a;
+	*a = *b;
+	*b = s;
 }

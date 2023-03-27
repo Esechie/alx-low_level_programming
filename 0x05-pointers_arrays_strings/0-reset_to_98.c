1435160816cd6eea726ea9ea4a246de0cc196356
Esechie <emmanuelesechie@gmail.com>
2023-03-27 07:36:55 -0700

    Replacing content of n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,11 @@
 
 /**
  * reset_to_98 -  Fuction to reset content of variable to 98
- * n: number of focus
+ * @n: number of focus
  * Return: Success
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;	
+	*n = 98;
 }
